59fc7143a8ddd75e33e50cfca3c3fab9542855ff
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-05-13 07:38:29 -0400

    Create feature test for dealership index, names of dealerships

---
+++ spec/features/dealerships/index_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'dealerships index page' do
+  before(:each) do
+    @dealership_1 = Dealership.create!(name: "Ed Voyles Ford", created_at: DateTime.new(2000, 2, 2, 7, 30, 15), updated_at: DateTime.new(2020, 3, 10, 12, 15, 30), open: true, max_car_capacity: 150)
+    @dealership_2 = Dealership.create!(name: "Jim Ellis BMW", created_at: DateTime.new(1996, 1, 12, 8, 15, 37), updated_at: DateTime.new(2021, 4, 8, 9, 25, 17), open: true, max_car_capacity: 100)
+  end
+
+  it 'can view the name of each dealership in the system' do
+    visit '/dealerships'
+
+    expect(page).to have_content(@dealership_1.name)
+    expect(page).to have_content(@dealership_2.name)
+  end
+end
