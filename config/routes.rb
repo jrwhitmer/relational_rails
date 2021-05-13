18c7589aa5904fd750665821e1aea96e4cc10a73
JoannaCoPo <jococopo@gmail.com>
2021-05-13 15:50:05 -0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/farmers_market_stands', to: 'farmers_market_stands#index'
+  get '/farmers_market_stands/:id', to: 'farmers_market_stands#show'
 end
