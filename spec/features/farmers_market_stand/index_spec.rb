75dda54fdc7d1d185b7620d7cb2c5498be769e3b
JoannaCoPo <jococopo@gmail.com>
2021-05-13 15:46:14 -0600

    Create test for index action

---
+++ spec/features/farmers_market_stand/index_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'farmers market stands index page', type: :feature do
+  it 'can see all attribute names' do
+    stand_1 = FarmersMarketStand.create!(name: "Red Wagon Organic Farm", city: "Boulder", seasonal: true, staffing: 3)
+    stand_2 = FarmersMarketStand.create!(name: "Devries Produce", city: "Olathe", seasonal: false, staffing: 5)
+
+    visit '/farmers_market_stands'
+
+    expect(page).to have_content(stand_1.name)
+    expect(page).to have_content(stand_2.name)
+  end
+end
