fad5db3e29381ee3ad5d5435fa71953661c74fe0
JoannaCoPo <jococopo@gmail.com>
2021-05-14 14:45:53 -0600

    Fix views for show

--- spec/features/farmers_market_stand/show_spec.rb
+++ spec/features/farmers_market_stand/show_spec.rb
@@ -1,8 +1,4 @@
 require 'rails_helper'
-#   As a visitor
-# When I visit '/parents/:id'
-# Then I see the parent with that id including the parent's attributes:
-# - data from each column that is on the parent table
 
 RSpec.describe 'farmers market stands index page' do
   it 'can display farmers market stands and their attributes on the id page' do
@@ -12,10 +8,10 @@ RSpec.describe 'farmers market stands index page' do
     visit "/farmers_market_stands/#{@stand_1.id}"
 
     expect(page).to have_content(@stand_1.name)
-    # expect(page).to have_content(stand_1.created_at)
-    # expect(page).to have_content(stand_1.updated_at)
-    # expect(page).to have_content(@stand_1.city)
-    # expect(page).to have_content(@stand_1.seasonal)
-    # expect(page).to have_content(@stand_1.staffing)
+    expect(page).to have_content(stand_1.created_at)
+    expect(page).to have_content(stand_1.updated_at)
+    expect(page).to have_content(@stand_1.city)
+    expect(page).to have_content(@stand_1.seasonal)
+    expect(page).to have_content(@stand_1.staffing)
   end
 end
