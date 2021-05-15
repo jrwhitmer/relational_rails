b68c35083218f05aee02d3aaeff457c23b89d267
JoannaCoPo <jococopo@gmail.com>
2021-05-14 20:37:12 -0600

    Add show action for produce_item

--- app/controllers/produce_items_controller.rb
+++ app/controllers/produce_items_controller.rb
@@ -2,4 +2,8 @@ class ProduceItemsController < ApplicationController
   def index
     @produce_items = ProduceItem.all
   end
+
+  def show
+    @produce_item = ProduceItem.find(params[:id])
+  end
 end
