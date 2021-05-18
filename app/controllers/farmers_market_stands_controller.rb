9cf683c30af7d6b7e82381e7f3cd5b1b4143a086
JoannaCoPo <jococopo@gmail.com>
2021-05-18 17:32:47 -0600

    Create new action

--- app/controllers/farmers_market_stands_controller.rb
+++ app/controllers/farmers_market_stands_controller.rb
@@ -1,9 +1,14 @@
 class FarmersMarketStandsController < ApplicationController
+
   def index
     @farmers_market_stands = FarmersMarketStand.all.order('created_at')
   end
 
   def show
     @farmers_market_stand = FarmersMarketStand.find(params[:id])
+  end
+
+  def new
+
   end
 end
