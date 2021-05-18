c4db92b97cbd3d991cf1571864d4c53134641c46
JoannaCoPo <jococopo@gmail.com>
2021-05-18 10:39:47 -0600

    Attempt to fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   get '/farmers_market_stands', to: 'farmers_market_stands#index'
   get '/farmers_market_stands/:id', to: 'farmers_market_stands#show'
   get '/produce_items', to: 'produce_items#index'
   get '/produce_items/:id', to: 'produce_items#show'
-  get '/farmers_market_stands/:id/produce_items', to: 'farmers_market_stands#show_produce_items_by_farmers_market_stand_id'
+  # get '/farmers_market_stands/:id/produce_items', to: 'farmers_market_stands#show_produce_items_by_farmers_market_stand_id'
+  get '/farmers_market_stands/:farmers_market_stand_id/produce_items', to: 'farmers_market_stand_produce_items#index'
+  get '/farmers_market_stands/:id/produce_items', to: 'dealerships#show_produce_items_by_farmers_market_stand_id'
+
   get '/dealerships', to: 'dealerships#index'
   get '/dealerships/:id', to: 'dealerships#show'
   get '/cars', to: 'cars#index'
