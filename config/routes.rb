4888ac3bc9955c8b8b18db6ecf76a3f9503465a4
JoannaCoPo <jococopo@gmail.com>
2021-05-19 12:56:00 -0600

    Add routes for to edit and update actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # get '/farmers_market_stands', to: 'farmers_market_stands#index'
-  # get '/farmers_market_stands/new', to: 'farmers_market_stands#new'
-  # post '/farmers_market_stands/new', to: 'farmers_market_stands#new'
-  # post '/farmers_market_stands', to: 'farmers_market_stands#create'
-  # get '/farmers_market_stands/:id', to: 'farmers_market_stands#show'
-  # get '/farmers_market_stands/:farmers_market_stand_id/produce_items', to: 'farmers_market_stand_produce_items#index'
-  # get '/produce_items', to: 'produce_items#index'
-  # get '/produce_items/:id', to: 'produce_items#show'
-
   get '/produce_items', to: 'produce_items#index'
   get '/produce_items/:id', to: 'produce_items#show'
   get '/farmers_market_stands/:farmers_market_stand_id/produce_items', to: 'farmers_market_stand_produce_items#index'
@@ -16,8 +7,9 @@ Rails.application.routes.draw do
   get '/farmers_market_stands/new', to: 'farmers_market_stands#new'
   post '/farmers_market_stands/new', to: 'farmers_market_stands#new'
   post '/farmers_market_stands', to: 'farmers_market_stands#create'
+  get '/farmers_market_stands/:id/edit', to: 'farmers_market_stands#edit'
+  patch '/farmers_market_stands/:id', to: 'farmers_market_stands#update'
   get '/farmers_market_stands/:id', to: 'farmers_market_stands#show'
-
   get '/dealerships', to: 'dealerships#index'
   get '/dealerships/:id', to: 'dealerships#show'
   get '/dealerships/:dealership_id/cars', to: 'dealership_cars#index'
