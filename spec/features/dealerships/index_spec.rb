76546c0ba63873bab7e00b6a0cc323c038b7df32
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-05-18 20:28:06 -0400

    Write tests for user story 11

--- spec/features/dealerships/index_spec.rb
+++ spec/features/dealerships/index_spec.rb
@@ -31,4 +31,27 @@ RSpec.describe 'dealerships index page' do
 
     expect(page).to have_link('Dealerships', href: '/dealerships')
   end
+
+  it 'has a link to create a new dealership form page' do
+    visit '/dealerships'
+
+    expect(page).to have_link('New Dealership', href: '/dealerships/new')
+  end
+
+  it 'takes user to dealership/new when new dealership button is clicked' do
+    visit '/dealerships'
+    click_button 'New Dealership'
+
+    expect(current_path).to be('/dealerships/new')
+  end
+
+  it 'can submit a filled in form for new dealership and redirects user to dealership index page' do
+    visit '/dealerships/new'
+    fill_in 'Name', with: 'Sydney Hardy Toyota'
+    fill_in 'Open', with: 'false'
+    fill_in 'Max Car Capacity', with: '50'
+    click_button 'Create Dealership'
+
+    expect(current_path).to be('/dealerships')
+  end
 end
